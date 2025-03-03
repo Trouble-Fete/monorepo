e3c57e54a54d10d0e7df3712c55de195c6d66b29
Trouble-Fete <neyroud.thomas@gmail.com>
2025-03-03 15:47:02 +0100

    quest 4 done

--- server/src/router.ts
+++ server/src/router.ts
@@ -14,10 +14,13 @@ router.get("/api/items/:id", itemActions.read);
 router.post("/api/items", itemActions.add);
 
 // Define program-related routes
+import categoryActions from "./modules/category/categoryActions";
 import programActions from "./modules/program/programActions";
 
 router.get("/api/programs", programActions.browse);
-
+router.get("/api/programs/:id", programActions.read);
+router.get("/api/categories", categoryActions.browse);
+router.get("/api/categories/:id", categoryActions.read);
 /* ************************************************************************* */
 
 // Declaration of a "Welcome" route
